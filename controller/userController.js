b851d33c67fee0e22398ae2f6db129976716a5ba
Elochukwu3 <oguajuromauld5@gmail.com>
2024-07-04 13:55:08 +0100

    clean up

--- controller/userController.js
+++ controller/userController.js
@@ -1,30 +1,22 @@
 const { handleGetLocation, handleGetTemperature } = require('../helper/index');
 
-
 const getUser = async (req, res) => {
-    const { visitor_name } = req.query
-
+    const { visitor_name } = req.query;
 
     if (!visitor_name) {
-        return res.send("Hey name is required")
+        return res.send("Hey name is required");
     }
 
     const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
 
-    const userLocation = await handleGetLocation(ip)
-
-    const userTemp = await handleGetTemperature(userLocation)
-
+    const userLocation = await handleGetLocation(ip);
+    const userTemp = await handleGetTemperature(userLocation);
 
     res.status(200).json({
-        clientIp: ip,
-        location: userLocation,
-        greeting: `Hello, ${visitor_name}!, the temperature is ${userTemp} degree Celcius in ${userLocation}`
-    })
-
-
+        "client_ip": ip,
+        "location": userLocation,
+        "greeting": `Hello, ${visitor_name}!, the temperature is ${userTemp} degrees Celsius in ${userLocation}`
+    });
 }
 
-
-module.exports = getUser
-
+module.exports = getUser;
